25ced7df815a42304d7ba31a2b0ced7f711bc4f2
FusionBoa <fusionboa@github.com>
2026-07-23 14:34:02 -0700

    v0.9.2: Cleanup — remove clutter (all_in_one examples, commit msgs, test runners, complete_test artifacts)

--- all_in_one.js
+++
@@ -1,8 +0,0 @@
-function update_results(data) {
-  let element = document.getElementById('results');
-  element.innerHTML = data;
-}
-function init() {
-  console.log('App initialized!');
-}
-init();
